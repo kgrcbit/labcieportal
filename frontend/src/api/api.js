ec055be4a20d9950aa6966b1cfe21a7fc387aa6a
kgrcbit <64593708+kgrcbit@users.noreply.github.com>
2025-11-17 13:52:17 +0530

    Update API URL to new endpoint

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://labcieportal-ppt1.onrender.com/api";
+const API_URL = "https://labcieportal.onrender.com/api";
 
 const API = axios.create({
   baseURL: API_URL,
